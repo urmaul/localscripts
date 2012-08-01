03d61d664fb4bc6b472578125a66e74c3f9513de
bogdan <bogdan@itechcraft.com>
2012-08-01 12:38:33 +0300

    "$" previx works

---
+++ LocalScriptsBehavior.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @property CClientScript $owner
+ */
+class LocalScriptsBehavior extends CBehavior
+{
+    public $jsDir  = '$js/';
+    public $cssDir = '$css/';
+    
+    public function attach($owner)
+    {
+        parent::attach($owner);
+        
+        $this->jsDir  = $this->setupPrefix($this->jsDir);
+        $this->cssDir = $this->setupPrefix($this->cssDir);
+    }
+
+
+    public function registerLocalScript($name, $position=0)
+    {
+        return $this->getOwner()->registerScriptFile($this->jsDir . $name, $position);
+    }
+    
+    public function registerLocalCss($name, $media='')
+    {
+        return $this->getOwner()->registerCssFile($this->cssDir . $name, $media);
+    }
+    
+    
+    private function setupPrefix($dir)
+    {
+        if ($dir[0] == '$') {
+            $dir = Yii::app()->baseUrl . substr($dir, 1);
+        }
+        
+        return $dir;
+    }
+}
