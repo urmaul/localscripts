31cad133e367f57373a2356b235bf4b8bdfc1411
urmaul <bogdan@itechcraft.com>
2013-02-12 15:25:56 +0200

    fake-clientscript added

---
+++ fake-clientscript.php
@@ -0,0 +1,12 @@
+<?php
+
+// DO NOT include this file!
+// It is created only to improve IDE autosuggest.
+
+/**
+ * @method CClientScript registerLocalScript($name, $position=0) register script file from your javascripts folder.
+ * @method CClientScript registerLocalCss($name, $media='') register css file from your styles folder.
+ */
+class CClientScript
+{
+}
