88aa3af0d29d142585193b63dc4f38ef92f26c91
urmaul <blacksilver@codesex.org>
2013-10-09 16:45:19 +0300

    phpdoc fix

--- fake-clientscript.php
+++ fake-clientscript.php
@@ -4,8 +4,8 @@
 // It is created only to improve IDE autosuggest.
 
 /**
- * @method CClientScript registerLocalScript($name, $position=0) register script file from your javascripts folder.
- * @method CClientScript registerLocalCss($name, $media='') register css file from your styles folder.
+ * @method CClientScript registerLocalScript($name, $position = 0) register script file from your javascripts folder.
+ * @method CClientScript registerLocalCss($name, $media = '') register css file from your styles folder.
  */
 class CClientScript
 {
